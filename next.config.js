b6384da31fd5a2088ea229ac4a4b6ec57b95337e
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2023-06-28 10:51:18 +0300

    pwa test

--- next.config.js
+++ next.config.js
@@ -4,16 +4,27 @@
 // module.exports = nextConfig
 
 
-const withPWA = require('next-pwa')
+// const withPWA = require('next-pwa')
+  
+//   module.exports = withPWA({
+//     // next.js config
+//     // reactStrictMode: true,
+//     pwa:{
+//         dist:"public",
+//         register: true,
+//     skipWaiting: true,
+//     }
+
+
+//   })
+
+
+  const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
   
   module.exports = withPWA({
     // next.js config
-    reactStrictMode: true,
-    pwa:{
-        dist:"public",
-        register: true,
+    register: true,
     skipWaiting: true,
-    }
-
-
   })
